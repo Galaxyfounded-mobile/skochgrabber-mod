a34d10038018f06db6e80b78e0897e5dc0a51c27
Galaxyfounded-mobile <ma.nameis160@gmail.com>
2024-08-12 22:12:51 +0300

    Update SkochOBF.py

--- Components/SkochOBF.py
+++ Components/SkochOBF.py
@@ -1,4 +1,10 @@
-import random, string, base64, codecs, argparse, os, sys
+import random
+import string
+import base64
+import codecs
+import argparse
+import os
+import sys
 from textwrap import wrap
 from lzma import compress
 from marshal import dumps
@@ -23,38 +29,35 @@ class SkochOBF:
         self.finalize()
 
     def generate(self, name):
-        res = self.vars.get(name)
-        if res is None:
-            res = "_" + "".join([random.choice(string.ascii_letters + string.digits) for _ in range(self.varlen)])
+        if name not in self.vars:
+            self.vars[name] = "_" + "".join(random.choices(string.ascii_letters + string.digits, k=self.varlen))
             self.varlen += 1
-            self.vars[name] = res
-        return res
+        return self.vars[name]
 
-    def encryptstring(self, string, config={}, func=False):
-        b64 = list(b"base64")
-        b64decode = list(b"b64decode")
+    def encryptstring(self, string, config=None, func=False):
+        if config is None:
+            config = {}
+        b64 = b"base64"
+        b64decode = b"b64decode"
         __import__ = config.get("__import__", "__import__")
         getattr = config.get("getattr", "getattr")
         bytes = config.get("bytes", "bytes")
         eval = config.get("eval", "eval")
+
         if not func:
-            return f'{getattr}({__import__}({bytes}({b64}).decode()), {bytes}({b64decode}).decode())({bytes}({list(base64.b64encode(string.encode()))})).decode()'
+            return (f'{getattr}({__import__}({bytes}({list(b64)}).decode()), '
+                    f'{bytes}({list(b64decode)}).decode())'
+                    f'({bytes}({list(base64.b64encode(string.encode()))})).decode()')
         else:
             attrs = string.split(".")
             base = self.encryptstring(attrs[0], config)
-            attrs = list(map(lambda x: self.encryptstring(x, config, False), attrs[1:]))
-            newattr = ""
-            for i, val in enumerate(attrs):
-                if i == 0:
-                    newattr = f'{getattr}({eval}({base}), {val})'
-                else:
-                    newattr = f'{getattr}({newattr}, {val})'
+            newattr = base
+            for attr in attrs[1:]:
+                newattr = f'{getattr}({eval}({newattr}), {self.encryptstring(attr, config)})'
             return newattr
 
     def encryptor(self, config):
-        def func_(string, func=False):
-            return self.encryptstring(string, config, func)
-        return func_
+        return lambda string, func=False: self.encryptstring(string, config, func)
 
     def compress(self):
         self.code = compress(self.code)
@@ -72,57 +75,56 @@ class SkochOBF:
 
     def encrypt1(self):
         code = base64.b64encode(self.code).decode()
-        partlen = int(len(code) / 4)
+        partlen = len(code) // 4
         code = wrap(code, partlen)
-        var1 = self.generate("a")
-        var2 = self.generate("b")
-        var3 = self.generate("c")
-        var4 = self.generate("d")
-        init = [f'{var1}="{codecs.encode(code[0], "rot13")}"', f'{var2}="{code[1]}"', f'{var3}="{code[2][::-1]}"', f'{var4}="{code[3]}"']
+        var1, var2, var3, var4 = [self.generate(name) for name in ("a", "b", "c", "d")]
 
+        init = [
+            f'{var1}="{codecs.encode(code[0], "rot13")}"',
+            f'{var2}="{code[1]}"',
+            f'{var3}="{code[2][::-1]}"',
+            f'{var4}="{code[3]}"'
+        ]
         random.shuffle(init)
-        init = ";".join(init)
-        self.code = f'''
-{init};__import__({self.encryptstring("builtins")}).exec(__import__({self.encryptstring("marshal")}).loads(__import__({self.encryptstring("base64")}).b64decode(__import__({self.encryptstring("codecs")}).decode({var1}, __import__({self.encryptstring("base64")}).b64decode("{base64.b64encode(b'rot13').decode()}").decode())+{var2}+{var3}[::-1]+{var4})))
-'''.strip().encode()
+        init_code = ";".join(init)
+        self.code = (f'{init_code};__import__({self.encryptstring("builtins")}).exec('
+                     f'__import__({self.encryptstring("marshal")}).loads('
+                     f'__import__({self.encryptstring("base64")}).b64decode('
+                     f'__import__({self.encryptstring("codecs")}).decode({var1}, '
+                     f'__import__({self.encryptstring("base64")}).b64decode("{base64.b64encode(b"rot13").decode()}").decode())+{var2}+{var3}[::-1]+{var4})))').encode()
 
     def encrypt2(self):
         self.compress()
-        var1 = self.generate("e")
-        var2 = self.generate("f")
-        var3 = self.generate("g")
-        var4 = self.generate("h")
-        var5 = self.generate("i")
-        var6 = self.generate("j")
-        var7 = self.generate("k")
-        var8 = self.generate("l")
-        var9 = self.generate("m")
+        varnames = [self.generate(name) for name in "efghijklm"]
 
         conf = {
-            "getattr": var4,
-            "eval": var3,
-            "__import__": var8,
-            "bytes": var9
+            "getattr": varnames[3],
+            "eval": varnames[2],
+            "__import__": varnames[7],
+            "bytes": varnames[8]
         }
         encryptstring = self.encryptor(conf)
 
-        self.code = f'''# https://dsc.gg/skochworld
-{var3} = eval({self.encryptstring("eval")});{var4} = {var3}({self.encryptstring("getattr")});{var8} = {var3}({self.encryptstring("__import__")});{var9} = {var3}({self.encryptstring("bytes")});{var5} = lambda {var7}: {var3}({encryptstring("compile")})({var7}, {encryptstring("<string>")}, {encryptstring("exec")});{var1} = {self.code}
-{var2} = {encryptstring('__import__("builtins").list', func=True)}({var1})
-try:
-    {encryptstring('__import__("builtins").exec', func=True)}({var5}({encryptstring('__import__("lzma").decompress', func=True)}({var9}({var2})))) or {encryptstring('__import__("os")._exit', func=True)}(0)
-except {encryptstring('__import__("lzma").LZMAError', func=True)}:...
-'''.strip().encode()
+        self.code = (f'# https://dsc.gg/skochworld\n'
+                     f'{varnames[2]} = eval({self.encryptstring("eval")});'
+                     f'{varnames[3]} = {varnames[2]}({self.encryptstring("getattr")});'
+                     f'{varnames[7]} = {varnames[2]}({self.encryptstring("__import__")});'
+                     f'{varnames[8]} = {varnames[2]}({self.encryptstring("bytes")});'
+                     f'{varnames[4]} = lambda {varnames[6]}: {varnames[2]}({encryptstring("compile")})({varnames[6]}, {encryptstring("<string>")}, {encryptstring("exec")});'
+                     f'{varnames[0]} = {self.code.decode()}\n'
+                     f'{varnames[1]} = {encryptstring("__import__(\'builtins\').list", func=True)}({varnames[0]})\n'
+                     f'try:\n'
+                     f'    {encryptstring("__import__(\'builtins\').exec", func=True)}({varnames[4]}({encryptstring("__import__(\'lzma\').decompress", func=True)}({varnames[8]}({varnames[1]}))))\n'
+                     f'except {encryptstring("__import__(\'lzma\').LZMAError", func=True)}:...').encode()
 
     def finalize(self):
-        if os.path.dirname(self.outpath).strip() != "":
-            os.makedirs(os.path.dirname(self.outpath), exist_ok=True)
+        os.makedirs(os.path.dirname(self.outpath), exist_ok=True)
         with open(self.outpath, "w") as e:
             e.write(self.code.decode())
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(prog=sys.argv[0], description="Obfuscates python program to make it harder to read")
-    parser.add_argument("FILE", help="Path to the file containing the python code")
+    parser = argparse.ArgumentParser(prog=sys.argv[0], description="Obfuscates a Python program to make it harder to read")
+    parser.add_argument("FILE", help="Path to the file containing the Python code")
     parser.add_argument("-o", type=str, help='Output file path [Default: "Obfuscated_<FILE>.py"]', dest="path")
     args = parser.parse_args()
 
@@ -130,7 +132,7 @@ if __name__ == "__main__":
         printerr(f'No such file: "{args.FILE}"')
         os._exit(1)
     elif not sourcefile.endswith((".py", ".pyw")):
-        printerr('The file does not have a valid python script extension!')
+        printerr('The file does not have a valid Python script extension!')
         os._exit(1)
 
     if args.path is None:
